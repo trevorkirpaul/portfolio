abf8859e238b71e781bc2a5e96f5b2f1bdfee4e3
Trevor Kirpaul <tkirpaul@gmail.com>
2017-10-30 13:36:11 -0400

    renamed public folder to docs for git pages

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 const livereload = require('livereload');
 const server = livereload.createServer();
 
-server.watch('public');
+server.watch('docs');
 
-app.use(express.static('public'));
+app.use(express.static('docs'));
 
 app.listen(3000, () => {
   console.log('Server running...');
