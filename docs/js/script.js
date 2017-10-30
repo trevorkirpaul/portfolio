2215376097688916638143cefca2934fa97564fd
Trevor Kirpaul <tkirpaul@gmail.com>
2017-10-30 14:49:41 -0400

    form fxn and setup

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,15 +1,21 @@
 console.log('Hello!');
-
+//hambruger menu vars
 const menu = document.getElementById('hamburger');
 const menuPanel = document.getElementById('mobileMenu');
-
 let bool = false;
+//form vars
+const inpName = document.getElementById('inpName');
+const inpSubject = document.getElementById('inpSubject');
+const txtMsg = document.getElementById('txtMessage');
+const btnClear = document.getElementById('btnClear');
+const btnSubmit = document.getElementById('btnSubmit');
 
+const formInputs = [inpName, inpSubject, txtMsg];
+
+
+//hamburger menu fxn
 menu.addEventListener('click', (event) => {
-
-  //console.log('works');
-  
-
+  //console.log('works'); 
   if (bool === false) {
     menuPanel.style.display = 'flex';
     bool = true;
@@ -18,9 +24,35 @@ menu.addEventListener('click', (event) => {
     menuPanel.style.display = 'none';
     bool = false;
     event.target.innerText = 'Menu';
-  }
-
-  
-
+  }  
 });
 
+//form fxn
+
+btnClear.addEventListener('click', () => {
+  event.preventDefault();
+})
+
+btnSubmit.addEventListener('click', () => {
+  event.preventDefault();
+})
+
+formInputs.forEach((item)=>{
+  let originalVal = item.value; 
+  let $bool = false;
+  item.addEventListener('click', () => {
+    
+    
+    if ($bool === false) {
+      event.target.value = '';
+    }
+    $bool = true;
+  });
+
+  item.addEventListener('focusout', () => {
+    if (event.target.value === '') {
+      event.target.value = originalVal;
+      $bool = false;
+    }
+  })
+})
